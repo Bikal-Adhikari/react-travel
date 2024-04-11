0f71dcedcba13346937e0f06c8732b607e58f490
Bikal <bikaladhikari5@gmail.com>
2024-04-11 17:01:16 +1000

    Navbar component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,78 +1,10 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
-      <nav className="navbar">
-        <div className="nav-center">
-          <div className="nav-header">
-            <img src="./images/logo.svg" className="nav-logo" alt="backroads" />
-            <button type="button" className="nav-toggle" id="nav-toggle">
-              <i className="fas fa-bars"></i>
-            </button>
-          </div>
-
-          <ul className="nav-links" id="nav-links">
-            <li>
-              <a href="#home" className="nav-link">
-                {" "}
-                home{" "}
-              </a>
-            </li>
-
-            <li>
-              <a href="#about" className="nav-link">
-                {" "}
-                about{" "}
-              </a>
-            </li>
-
-            <li>
-              <a href="#services" className="nav-link">
-                {" "}
-                services{" "}
-              </a>
-            </li>
-
-            <li>
-              <a href="#tours" className="nav-link">
-                {" "}
-                tours
-              </a>
-            </li>
-          </ul>
-
-          <ul className="nav-icons">
-            <li>
-              <a
-                href="https://www.twitter.com"
-                target="_blank"
-                className="nav-icon"
-              >
-                <i className="fab fa-facebook"></i>
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://www.twitter.com"
-                target="_blank"
-                className="nav-icon"
-              >
-                <i className="fab fa-twitter"></i>
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://www.twitter.com"
-                target="_blank"
-                className="nav-icon"
-              >
-                <i className="fab fa-squarespace"></i>
-              </a>
-            </li>
-          </ul>
-        </div>
-      </nav>
+      <Navbar />
 
       <section className="hero" id="home">
         <div className="hero-banner">
